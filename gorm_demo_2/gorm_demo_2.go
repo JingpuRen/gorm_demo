7a770d43441ac8b9b99f55025247aa6871c84186
JingpuRen <yy5396681@qq.com>
2025-02-16 13:31:34 +0800

    update

--- gorm_demo_2/gorm_demo_2.go
+++ gorm_demo_2/gorm_demo_2.go
@@ -28,7 +28,27 @@ type Animal struct {
 	Age      int64
 }
 
+// tip 列名由字段名称进行下划线分割来生成
+type Student struct {
+	ID        uint      // column name is `id`
+	Name      string    // column name is `name`
+	Birthday  time.Time // column name is `birthday`
+	CreatedAt time.Time // column name is `created_at`
+}
+
+// tip也可以使用结构体tag来指定列名
+type Teacher struct {
+	AnimalId int64     `gorm:"column:beast_id"`         // set column name to `beast_id`
+	Birthday time.Time `gorm:"column:day_of_the_beast"` // set column name to `day_of_the_beast`
+	Age      int64     `gorm:"column:age_of_the_beast"` // set column name to `age_of_the_beast`
+}
+
 func main() {
+	// tip 修改默认的表名
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return "JingpuRen_" + defaultTableName
+	}
+
 	db, err := gorm.Open("mysql", "root:666666@(localhost:3306)/db1?charset=utf8mb4&parseTime=True")
 
 	if err != nil {
@@ -42,4 +62,7 @@ func main() {
 
 	// 创建动物表
 	db.AutoMigrate(&Animal{})
+
+	// tip 如果不想自动创建结构体名称复数的表，我们可以通过指定创建的表的名称
+	// db.Table("animal").CreateTable(&Animal{})
 }
