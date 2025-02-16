2e0e62310d488f2d7ef40830f54da5f8f8c4c17e
JingpuRen <yy5396681@qq.com>
2025-02-16 13:05:03 +0800

    update

--- gorm_demo_2/gorm_demo_2.go
+++ gorm_demo_2/gorm_demo_2.go
@@ -8,7 +8,8 @@ import (
 )
 
 type User struct {
-	gorm.Model
+	// gorm默认将ID字段作为逐渐
+	gorm.Model   // 这个里面就有id字段
 	Name         string
 	Age          sql.NullInt64
 	Birthday     *time.Time
@@ -18,6 +19,13 @@ type User struct {
 	Num          int     `gorm:"AUTO_INCREMENT"`  // 设置 num 为自增类型
 	Address      string  `gorm:"index:addr"`      // 给address字段创建名为addr的索引
 	IgnoreMe     int     `gorm:"-"`               // 忽略本字段
+}
+
+// tip 如果我们想自己设置别的主键，我们可以通过tag来设置
+type Animal struct {
+	AnimalId int64 `gorm:"primary_key"` // 表示将这个字段设置为主键
+	Name     string
+	Age      int64
 }
 
 func main() {
@@ -31,4 +39,7 @@ func main() {
 
 	// 创建表，将表中的字段和代码结构体中的字段进行对应
 	db.AutoMigrate(&User{})
+
+	// 创建动物表
+	db.AutoMigrate(&Animal{})
 }
