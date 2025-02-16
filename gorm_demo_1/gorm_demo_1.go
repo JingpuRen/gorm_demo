0f8decdaf3f3bef8ee8ce191b3fd3f505d523db4
JingpuRen <yy5396681@qq.com>
2025-02-16 12:13:53 +0800

    update

--- gorm_demo_1/gorm_demo_1.go
+++ gorm_demo_1/gorm_demo_1.go
@@ -1,5 +1,24 @@
 package main
 
+import "github.com/jinzhu/gorm"
+
+type UserInfo struct {
+	Id     uint
+	Name   string
+	Gender string
+	Hobby  string
+}
+
 func main() {
+	// tip 连接MySQL数据库
+	db, err := gorm.Open("mysql", "root:666666@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=local")
+	if err != nil {
+		// tip 这段代码还有点不懂
+		panic(err)
+	}
+	defer db.Close()
+
+	// tip 创建表，将数据库表的字段和结构体中的字段进行一一对应
+	db.AutoMigrate(&UserInfo{})
 
 }
