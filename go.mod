7e7434445ad3225875598fb0dd69e96ebeb59ffb
JingpuRen <yy5396681@qq.com>
2025-02-16 12:37:00 +0800

    update

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module gorm_demo
 
 go 1.24.0
 
+require github.com/jinzhu/gorm v1.9.16
+
 require (
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 )
--- gorm_demo_1/gorm_demo_1.go
+++ gorm_demo_1/gorm_demo_1.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	// tip 一定要记得引入对应的数据库驱动啊！！！
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 type UserInfo struct {
 	Id     uint
@@ -11,14 +15,20 @@ type UserInfo struct {
 
 func main() {
 	// tip 连接MySQL数据库
-	db, err := gorm.Open("mysql", "root:666666@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=local")
+	// todo 这个时区设立的还有问题，后续还待解决
+	db, err := gorm.Open("mysql", "root:666666@(localhost:3306)/db1?charset=utf8mb4&parseTime=True")
 	if err != nil {
 		// tip 这段代码还有点不懂
 		panic(err)
+		//fmt.Println("数据库连接异常")
+		//return
 	}
 	defer db.Close()
 
 	// tip 创建表，将数据库表的字段和结构体中的字段进行一一对应
 	db.AutoMigrate(&UserInfo{})
+
+	u1 := UserInfo{1, "JingpuRen", "男", "coding"}
+	db.Create(&u1)
 
 }
